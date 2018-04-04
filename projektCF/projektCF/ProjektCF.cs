650c75e6bee377b040816a08ebc5f72a2f5617d9
Christiane Flohr <christiane.flohr@srh.de>
2018-04-04 13:43:55 +0200

    Funktionen in projektcf

--- projektCF/projektCF/ProjektCF.cs
+++ projektCF/projektCF/ProjektCF.cs
@@ -8,6 +8,25 @@ namespace projektCF
 {
     public class ProjektCF
     {
+        public class ProjektCF_mit_Test
+        {
+            public static List<int> Start_Zahlen_testen(int untergrenze, int obergrenze)
+            {
+                var zahlen = Zahlen_testen(untergrenze, obergrenze);
+                return zahlen;
+            }
+        }
 
+        private static List<int> Zahlen_testen(int untergrenze, int obergrenze)
+        {
+            var zahlen = new List<int>();
+
+            for (int zahl = untergrenze; zahl <= obergrenze; zahl++)
+            {
+                zahlen.Add(zahl);
+            }
+
+            return zahlen;
+        }
     }
 }
